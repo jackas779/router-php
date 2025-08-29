84ff984a6e3e67429bf2f1be45760d451042fb9e
Nicolas Ardila <jackas779@gmail.com>
2025-08-29 11:39:54 -0500

    se modifica forma de guardar rutas y de buscarlas.

--- src/Models/Route.php
+++ src/Models/Route.php
@@ -7,7 +7,7 @@ class Route
   private static array $routes = [];
   private static Router $router;
 
-  public static function run(string $method, array|object $param){
+  public static function run(string $method, array|callable $param){
 
     foreach (self::$routes as $route) {
 
@@ -33,20 +33,20 @@ class Route
     
   }
 
-  public static function get(string $url,array|object $param) {
+  public static function get(string $url,array|callable  $param) {
 
     $method = 'GET';
-    self::setRoutes($url,  $method);
+    self::setRoutes($url, $method, $param);
 
     if($_SERVER['REQUEST_METHOD'] !== $method) return;
 
     self::run($method,$param);
   }
 
-  public static function post(string $url,array|object $param) {
+  public static function post(string $url,array|callable  $param) {
 
     $method = 'POST';
-    self::setRoutes($url,  $method);
+    self::setRoutes($url, $method, $param);
 
     if($_SERVER['REQUEST_METHOD'] !== $method) return; 
 
@@ -58,9 +58,27 @@ class Route
     }
   }
 
-  private static function setRoutes(string $url, string $method) :void {
-    if(!in_array(['url' => $url, 'method'=> $method],self::$routes)){
-      self::$routes[] = ['url' => $url, 'method'=> $method];
+  private static function setRoutes(string $url, string $method, array|callable $param ) :void {
+
+    $action = null;
+
+    if(is_array($param)){
+      $action = join('@', $param);
+    }
+
+    $result = array_filter(self::$routes,function($route) use ($url, $method){
+      return $route['url'] === $url && $route['method'] === $method;
+    });
+
+    // if(!in_array(['url' => $url, 'method'=> $method],self::$routes)){
+    // }
+
+    if(empty($result)){
+      self::$routes[] = ['url' => $url, 'method'=> $method, 'action' => $action];
+    }else{
+      $url_columns = array_column(self::$routes, 'url');
+      $key = array_search($url, $url_columns);
+      self::$routes[$key]['action'] = $param; 
     }
   }
 
